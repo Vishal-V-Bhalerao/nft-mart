46f1870ff27edf7a07a75404e3007a0924b47cfa
Vishal Bhalerao <vishalbhalerao.bhalerao@gmail.com>
2022-01-19 11:58:46 +0530

    created server for nft list

---
+++ server/app.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+const path = require('path');
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const port = 8001;
+
+app.get('/nft', (req, res) => {
+    res.sendFile(path.join(__dirname, 'nft-list.json'));
+});
+
+app.listen(port, () => {
+    console.log(`[products] API listening on port ${port}.`);
+});
