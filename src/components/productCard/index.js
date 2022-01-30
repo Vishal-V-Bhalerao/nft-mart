2f1292c602ad3bd8d2139b32b2cf2e5c3e65c420
Vishal Bhalerao <vishalbhalerao.bhalerao@gmail.com>
2022-01-30 23:16:58 +0530

    card css changes

--- src/components/productCard/index.js
+++ src/components/productCard/index.js
@@ -11,39 +11,39 @@ const ProductCard = ({ product }) => {
         <div
             onMouseEnter={() => setBuyOption(true)}
             onMouseLeave={() => setBuyOption(false)}
-            className="grid grid-rows-4  rounded-lg primary-card card-size mb-5 shadow-md ">
-            <div className="row-span-3 flex-1 justify-center items-center rounded-t-lg overflow-hidden" >
+            className=" rounded-lg primary-card card-size mb-8 shadow-md ">
+            <div className=" min-img-size flex bg-black justify-center items-center rounded-t-lg overflow-hidden" >
                 <img className="shrink-0 w-full h-full" src={product.imageUrl}></img>
             </div>
-            <div className="row-span-1 rounded-b-lg flex  flex-col" >
+            <div className=" flex flex-col rounded-b-lg " >
                 <div className="h-16 flex p-3">
                     <div className="w-3/5 " >
                         <div className="flex justify-items-center" >
-                            <div className="text-xs font-medium overflow-ellipsis collection-name">
+                            <div className="card-text-xs font-medium overflow-ellipsis collection-name">
                                 <span title={product.collection.name} >{product.collection.name}</span>
                             </div>
                         </div>
                         <div>
-                            <div className="text-xs font-semibold product-name">
+                            <div className="card-text-xs font-semibold product-name">
                                 <span title={product.name} >{product.name}</span>
                             </div>
                         </div>
                     </div>
                     <div className="w-2/5 flex flex-col items-end " >
                         <div className="flex justify-items-center" >
-                            <div className="text-xs font-medium overflow-ellipsis collection-name">
+                            <div className="card-text-xs font-medium overflow-ellipsis collection-name">
                                 <span >Price</span>
                             </div>
                         </div>
                         <div>
-                            <div className="text-sm font-semibold  product-name flex ">
+                            <div className="card-text-sm font-semibold  product-name flex ">
                                 <img className="w-4 h-4" src={product.symbolImageUrl}></img>
                                 <span >{product.coinPrice}</span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="flex justify-between pl-3 pr-3">
+                <div className="flex justify-between pt-6 pl-3 pr-3">
                     {buyOption ? <div className="text-sm font-semibold primary-blue"> Buy now </div> : <div></div>}
                     <div className="collection-name text-lg" >
                         <FontAwesomeIcon icon={faHeart} ></FontAwesomeIcon>
